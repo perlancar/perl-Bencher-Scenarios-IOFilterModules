2aa4ca0b0c9735dbf706646b7cdca8dc63e28557
perlancar (@pc-office) <perlancar@gmail.com>
2016-10-12 11:02:16 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/IOFilterModules/Writing.pm
@@ -0,0 +1,79 @@
+package Bencher::Scenario::IOFilterModules::Writing;
+
+# DATE
+# VERSION
+
+use strict;
+use warnings;
+use File::Temp qw(tempfile);
+
+our $scenario = {
+    summary => 'Benchmark writing with filter that does nothing',
+
+    description => <<'_',
+
+Each participant will write `chunk_size` bytes (0, 1, and 1024) for 1000 times.
+
+_
+
+    modules => {
+    },
+
+    participants => [
+        {
+            module => 'Tie::Handle::Filter',
+            code_template => <<'_',
+open *FH0, ">", <tempfile> or die "Can't open: $!";
+tie  *FH , "Tie::Handle::Filter", *FH0, sub { @_ };
+my $chunk = "a" x <chunk_size>;
+for (1..1000) { print FH $chunk }
+close FH;
+die "Incorrect file size" unless (-s <tempfile>) == <chunk_size> * 1000;
+_
+        },
+        {
+            name => 'PerlIO::via',
+            module => 'PerlIO::via::as_is',
+            code_template => <<'_',
+open my($fh), ">:via(as_is)", <tempfile>;
+my $chunk = "a" x <chunk_size>;
+for (1..1000) { print $fh $chunk }
+close $fh;
+die "Incorrect file size" unless (-s <tempfile>) == <chunk_size> * 1000;
+_
+        },
+        {
+            name => 'raw',
+            code_template => <<'_',
+open my($fh), ">", <tempfile>;
+my $chunk = "a" x <chunk_size>;
+for (1..1000) { print $fh $chunk }
+close $fh;
+die "Incorrect file size" unless (-s <tempfile>) == <chunk_size> * 1000;
+_
+        },
+    ],
+
+    # generate datasets
+    before_list_datasets => sub {
+        my %args = @_;
+        my $scenario = $args{scenario};
+        my $seq = 0;
+        for my $chunk_size (0, 1, 1024) {
+            my ($fh, $filename) = tempfile();
+            push @{ $scenario->{datasets} }, {
+                name => "chunk_size=$chunk_size",
+                args => {chunk_size => $chunk_size, tempfile => $filename},
+                seq => $seq++,
+            };
+        }
+    },
+
+    # generated dynamically
+    datasets => undef,
+};
+
+1;
+# ABSTRACT:
+
+=head1 SEE ALSO
---
+++ lib/Bencher/Scenarios/IOFilterModules.pm
@@ -0,0 +1,9 @@
+package Bencher::Scenarios::IOFilterModules;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Scenarios to benchmark I/O filtering modules
+
+=head1 SEE ALSO
